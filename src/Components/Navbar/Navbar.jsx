6f639e2773e6d5f15fd0de7248e76b21feae51fd
Shafia Naveed <navidkhan5272@gmail.com>
2025-11-04 16:22:13 -0800

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='h-[101px] w-[1440px] bg-gray-500'>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
